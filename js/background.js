2414531c071c29ea0367239f63d4a6507c58eb2b
Mark <mark.sanjuan.sole@gmail.com>
2020-11-02 23:30:17 +0100

    working on it

--- js/background.js
+++ js/background.js
@@ -1,7 +1,6 @@
-    // CREACIÓN DEL FONDO
-    
-    
-    const background = new BackgroundColor(canvas.width, hGround, 0, canvas.height - hGround, colorGround)
+// CREACIÓN DEL FONDO
 
-    //  ----------- CREACIÓN DE FONDO PARA ANIMACIÓN ------------
-    const backgroundAnimation = new BackgroundColor(canvas.width, canvas.height, 0, 0, 'rgb(255, 0, 0, 0.8)')
+const background = new BackgroundColor(canvas.width, hGround, 0, canvas.height - hGround, colorGround)
+
+//  ----------- CREACIÓN DE FONDO PARA ANIMACIÓN ------------
+const backgroundAnimation = new BackgroundColor(canvas.width, canvas.height, 0, 0, 'rgb(255, 0, 0, 0.8)')
